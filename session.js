ffe2484860fc2451771b49a98ef4e7b2bda389e7
rashmikapramodsamarasinghe <rashmikapramod541@gmail.com>
2024-11-24 17:49:03 +0530

    Update session.js
    Pleas

--- session.js
+++ session.js
@@ -21,5 +21,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session_id here",
+SESSION_ID: process.env.SESSION_ID || "ANJU-MD=LNEzlarK#mBIMfnO54J8skkkI5P8Pf45lUV5pY4mFFc8a_J1osww",
 };
